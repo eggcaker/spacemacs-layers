60e879a653469848b06abefc06b17ec53e57ecc9
eggcaker <eggcaker@gmail.com>
2016-03-03 18:35:45 +0800

    changed some org-custom-commands

--- init.el
+++ init.el
@@ -430,19 +430,3 @@ Consider only documented, non-obsolete functions."
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(exec-path-from-shell-variables (quote ("PATH")))
- '(org-gcal-dir "~/.emacs.d/.cache/org-gcal/" t)
- '(org-gcal-logo "org.png" t)
- '(org-gcal-token-file "~/.emacs.d/.cache/.org-gcal-token" t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(company-tooltip-common ((t (:inherit company-tooltip :weight bold :underline nil))))
- '(company-tooltip-common-selection ((t (:inherit company-tooltip-selection :weight bold :underline nil)))))
