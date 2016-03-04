7b2612ae0382ed1e288be992aab8cb4d07595e46
eggcaker <eggcaker@gmail.com>
2016-03-04 11:04:26 +0800

    added some state change hook

--- pelm-org/packages.el
+++ pelm-org/packages.el
@@ -63,6 +63,34 @@
         "oO" 'pelm-org/punch-out))
     :config
     (progn
+
+      ;; start of test code
+      (defun wicked/org-clock-in-if-starting ()
+        "Clock in when the task is marked STARTED."
+        (when (and (string= org-state "STARTED")
+                   (not (string= org-last-state org-state)))
+          (org-clock-in)))
+      (add-hook 'org-after-todo-state-change-hook
+                'wicked/org-clock-in-if-starting)
+      (defadvice org-clock-in (after wicked activate)
+        "Set this task's status to 'STARTED'."
+        (org-todo "STARTED"))
+      (defun wicked/org-clock-out-if-waiting ()
+        "Clock out when the task is marked WAITING."
+        (when (and (string= org-state "WAITING")
+                   (equal (marker-buffer org-clock-marker) (current-buffer))
+                   (< (point) org-clock-marker)
+                   (> (save-excursion (outline-next-heading) (point))
+                      org-clock-marker)
+                   (not (string= org-last-state org-state)))
+          (org-clock-out)))
+      (add-hook 'org-after-todo-state-change-hook
+                'wicked/org-clock-out-if-waiting)
+
+
+      ;; end of test code
+
+
       ;; just for fun
       (setq org-agenda-category-icon-alist
             '(("[Ee]macs" "~/.spacemacs.d/pelm-org/vendor/icons/org/emacs.png" nil nil :ascent center)
