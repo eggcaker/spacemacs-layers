5d7e023109fb0f15d640903cb45c0760b48b7ec7
eggcaker <eggcaker@gmail.com>
2017-05-16 08:23:43 +0800

    Add Typescript support

--- init.el
+++ init.el
@@ -31,6 +31,7 @@ values."
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
    '(
+     typescript
      ruby
      csv
      vimscript
@@ -213,7 +214,7 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(spacemacs-dark spacemacs-light  dracula  solarized)
+   dotspacemacs-themes '(spacemacs-dark spacemacs-light rebecca )
    ;; If non-nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
