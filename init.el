32eb17637115a66ec3b705ee46f7424d795e2059
eggcaker <eggcaker@gmail.com>
2017-09-07 12:02:41 +0800

    added vimscript layer

--- init.el
+++ init.el
@@ -30,7 +30,8 @@ values."
     dotspacemacs-configuration-layer-path '()
     ;; List of configuration layers to load.
     dotspacemacs-configuration-layers
-    '(vimscript
+    '(javascript
+			 vimscript
 			 python
 			 nginx
        (typescript :variables
