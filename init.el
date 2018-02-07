a5eeb403c6b97ba8b0aac5d1dbc0bc08bb6c4188
eggcaker <eggcaker@gmail.com>
2018-02-07 09:01:54 +0800

    2018-02-07 auto commit

--- init.el
+++ init.el
@@ -29,7 +29,8 @@ This function should only modify configuration layer settings."
    dotspacemacs-configuration-layer-path '()
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
-   '(html
+   '(react
+     html
      ;; vimscript
      ;; clojure
      ;; react
