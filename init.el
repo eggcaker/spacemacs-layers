5015dece768153b98a97c6f45498907d6e49d3bf
eggcaker <eggcaker@gmail.com>
2019-02-12 19:15:08 +0800

    Daily commit 2019-02-12

--- init.el
+++ init.el
@@ -628,7 +628,7 @@ It should only modify the values of Spacemacs settings."
 (defun dotspacemacs/user-config ()
   (setq calendar-week-start-day 1)
   (setq org-reveal-root "http://cdn.jsdelivr.net/reveal.js/3.0.0/")
-  (setq python-indent-guess-indent-offset nil)
+  (setq python-indent-guess-indent-oftpfset nil)
   (evil-declare-change-repeat 'company-complete)
   (setq counsel-osx-app-location '("/Applications" "/Applications/Utilities"))
   (setq python-shell-interpreter "python")
